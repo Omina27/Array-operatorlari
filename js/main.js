0a19648060661c0813c92fdbdc1a5755b466aa1a
omina0127 <abdusattorovaomina032@gmail.com>
2021-11-19 17:15:28 +0500

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var data = ["Jazz", "Blues"]
+console.log(data);
+
+data.push("Roll-n-roll")
+console.log(data);
+
+data.splice(1, 1, "Classics")
+console.log(data);
+
+data.shift("Jazz")
+console.log(data);
+
+data.unshift("Rap", "Reggage")
+console.log(data);
